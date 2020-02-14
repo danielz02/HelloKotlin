8c61db96063f1b4faf8237c7b7f0e8b8d8421fa9
danielz02 <zchdz0215@gmail.com>
2020-02-14 14:38:56 -0600

    Add test case for Main and fix error message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,3 +7,7 @@ fun add(first: Int, second: Int): Int {
     require(first >= 0) {"first should be positive!"}
     return first + second
 }
+
+fun hello(): String {
+    return "Hello, World!"
+}
--- src/test/kotlin/TestMain.kt
+++ src/test/kotlin/TestMain.kt
@@ -2,9 +2,7 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class TestMain : StringSpec({
-    "test the add method" {
-        assert(add(5, 6) == 11)
-        require(add(0, 1) == 1)
-        add(5, 6) shouldBe 11
+    "should greet properly" {
+        hello() shouldBe "Hello, World!"
     }
 })
